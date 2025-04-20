6fd20a363afa7a1f9c4f362abe57fed46eb8bd94
ZachNg01 <154305798+ZachNg01@users.noreply.github.com>
2025-04-20 21:42:21 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const burger   = document.querySelector('.burger');
+const navMenu  = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-active');
+  navLinks.forEach((link, i) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${i/7 + 0.3}s`;
+    }
+  });
+  burger.classList.toggle('toggle');
+});
