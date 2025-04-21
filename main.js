f80f7db350fe46cc6baeb40fadae52d46e8b6018
ZachNg01 <154305798+ZachNg01@users.noreply.github.com>
2025-04-21 22:56:22 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-// Toggle mobile menu
 document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger');
-  const nav    = document.querySelector('.nav-links');
-  burger.addEventListener('click', () => nav.classList.toggle('open'));
-});
+  const burger   = document.getElementById('burger'),
+        navLinks = document.getElementById('nav-links');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const burger   = document.getElementById('burger');
-  const navLinks = document.getElementById('nav-links');
   burger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
   });
